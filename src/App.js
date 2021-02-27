c36e372f425e749afbcc71849d8331616650a811
Kazuki Okugawa <droitok@gmail.com>
2021-02-28 00:32:33 +0900

    hello world

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello world</p>
-      </header>
-    </div>
+    <h1>Hello world!</h1>
   );
 }
 
